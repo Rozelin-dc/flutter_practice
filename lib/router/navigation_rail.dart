792cefabf6acb997fb31216015d554828504445e
Rozelin <rozelin.dc@gmail.com>
2025-05-11 11:28:30 +0900

    NavigationRailをhooksで書き直した

--- lib/router/navigation_rail.dart
+++ lib/router/navigation_rail.dart
@@ -1,43 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 
 import 'router.dart';
 
-class MyNavigationRail extends StatelessWidget {
+class _RailItem {
+  const _RailItem({
+    required this.item,
+    required this.path,
+  });
+
+  final NavigationRailDestination item;
+  final String path;
+}
+
+class MyNavigationRail extends HookWidget {
   const MyNavigationRail({required this.child, super.key});
 
   final Widget child;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-        body: Row(
-          children: [
-            SafeArea(
-              child: NavigationRail(
-                backgroundColor: theme.colorScheme.surfaceContainer,
-                extended: constraints.maxWidth >= 600,
-                destinations: _railItems.map((item) => item.item).toList(),
-                selectedIndex: _calcSelectedIndex(context),
-                onDestinationSelected: (index) {
-                  _onDestinationSelected(context, index);
-                },
-              ),
-            ),
-            Expanded(
-              child: Container(
-                color: theme.colorScheme.primaryContainer,
-                child: child,
-              ),
-            ),
-          ],
-        ),
-      );
-    });
-  }
 
   final _railItems = const [
     _RailItem(
@@ -66,25 +46,41 @@ class MyNavigationRail extends StatelessWidget {
         path: MyPath.example),
   ];
 
-  int _calcSelectedIndex(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     final path = GoRouterState.of(context).fullPath;
-    return _railItems.indexWhere((item) => item.path == path);
+
+    final selectedIndex = useMemoized(() => _railItems.indexWhere((item) => item.path == path), [path]);
+    final onDestinationSelected = useCallback((int index) {
+      final newPath = _railItems[index].path;
+      if (newPath.isNotEmpty) {
+        GoRouter.of(context).push(newPath);
+      }
+    }, [context]);
+
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                backgroundColor: theme.colorScheme.surfaceContainer,
+                extended: constraints.maxWidth >= 600,
+                destinations: _railItems.map((item) => item.item).toList(),
+                selectedIndex: selectedIndex,
+                onDestinationSelected: onDestinationSelected,
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: theme.colorScheme.primaryContainer,
+                child: child,
+              ),
+            ),
+          ],
+        ),
+      );
+    });
   }
-
-  void _onDestinationSelected(BuildContext context, int index) {
-    final path = _railItems[index].path;
-    if (path.isNotEmpty) {
-      GoRouter.of(context).push(path);
-    }
-  }
-}
-
-class _RailItem {
-  const _RailItem({
-    required this.item,
-    required this.path,
-  });
-
-  final NavigationRailDestination item;
-  final String path;
 }
